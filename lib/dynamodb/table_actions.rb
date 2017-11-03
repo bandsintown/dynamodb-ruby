71c436a0359069fd4e22a3707641237f131b2c6f
Steve Farrow <sfarrow@bandsintown.com>
2017-11-03 09:39:35 -0700

    updated table actions

--- lib/dynamodb/table_actions.rb
+++ lib/dynamodb/table_actions.rb
@@ -47,7 +47,7 @@ module Dynamodb
 
     def delete_table(_table_name)
       # To prevent accidentally deleting tables in production
-      (raise "Can't delete tables in production") unless self.class.name == "Dynamodb" &&
+      (raise "Can't delete tables in production") unless self.name == "Dynamodb" &&
         self.config == { endpoint: "http://localhost:10070" }
 
       client.delete_table(table_name: _table_name)
@@ -55,7 +55,7 @@ module Dynamodb
 
     def create_table(_table_name, options)
       # To prevent accidentally creating tables in production
-      (raise "Can't create tables in production") unless self.class.name == "Dynamodb" &&
+      (raise "Can't create tables in production") unless self.name == "Dynamodb" &&
         self.config == { endpoint: "http://localhost:10070" }
 
       resource.create_table(
