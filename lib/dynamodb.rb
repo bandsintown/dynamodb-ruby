12879c104745a4712f54a5b709b20807c21d74de
Steve Farrow <sfarrow@bandsintown.com>
2017-11-09 17:03:43 -0800

    adding config options

--- lib/dynamodb.rb
+++ lib/dynamodb.rb
@@ -7,4 +7,22 @@ require "dynamodb/table_actions"
 module Dynamodb
   extend Connection
   extend TableActions
+
+  # class << self
+  #   attr_accessor :configuration
+  # end
+  #
+  # def self.configure
+  #   self.configuration ||= Configuration.new
+  #   yield(configuration)
+  # end
+  #
+  # class Configuration
+  # attr_accessor :client_settings, :resource_settings
+  #
+  #   def initialize
+  #     @client_settings = {}
+  #     @resource_settings = {}
+  #   end
+  # end
 end
--- spec/dynamodb_spec.rb
+++ spec/dynamodb_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Dynamodb do
   it "has a version number" do
     expect(Dynamodb::VERSION).to eq "0.3.0"
   end
+
+  # describe "#configure" do
+  #   before do
+  #     Dynamodb.configure do |config|
+  #       config.client_settings = { endpoint: 'www.dynamodb.com' }
+  #     end
+  #   end
+  #
+  #   it "returns the correct client settings" do
+  #     dynamodb = Dynamodb::Base.client_settings
+  #
+  #     expect(dynamodb.client_settings).to eq({ endpoint: 'www.dynamodb.com' })
+  #   end
+  # end
 end
